deaca263fd0922ece608949f0509b71470f2c80c
niranjan <mohan.niranjan@gmail.com>
2014-12-01 15:15:19 -0500

    Server thread impl

---
+++ src/com/aeh/thread/impl/ServerThreadImpl.java
@@ -0,0 +1,32 @@
+package com.aeh.thread.impl;
+
+import com.aeh.commonobjects.Handler;
+import com.aeh.thread.ServerThread;
+
+public class ServerThreadImpl implements ServerThread{
+
+	@Override
+	public void executeHandler(Handler handler) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkHandlerPQ() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Handler getHandler(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkQ() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
