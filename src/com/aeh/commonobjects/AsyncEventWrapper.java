528ee37428e3a8f08214a68972cfbd1c64573975
Karnesh Mehra <karneshmehra@gmail.com>
2014-12-03 22:22:03 -0500

    handling priority parameters for AsyncEventWrapper

--- src/com/aeh/commonobjects/AsyncEventWrapper.java
+++ src/com/aeh/commonobjects/AsyncEventWrapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.realtime.AsyncEvent;
 import javax.realtime.AsyncEventHandler;
+import javax.realtime.PriorityParameters;
 
 import com.aeh.AEHHolder;
 
@@ -12,17 +13,27 @@ import com.aeh.AEHHolder;
 public class AsyncEventWrapper  extends AsyncEvent{
 	List<AsyncEventHandler> handlers;
 	AEHHolder aehHolder;
+	int priority;
 
 
-	public AsyncEventWrapper() {
+	public AsyncEventWrapper(int priority) {
 		//aehHolder = 
 		handlers = new ArrayList<AsyncEventHandler>();
+		this.priority = priority;
 		aehHolder = AEHHolder.getInstance();
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	private void changePriority(AsyncEventHandler handler){
+		PriorityParameters sp = new PriorityParameters();
+		sp.setPriority(this.priority);
+		handler.setSchedulingParameters(sp);
+	}
+	
 
 	public void addHandler(AsyncEventHandler handler){
+		this.changePriority(handler);
 		synchronized(AsyncEventWrapper.class){
 			handlers.add(handler);
 		}
